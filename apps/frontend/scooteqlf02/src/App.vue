6394bdbed916c61531272eb842866b3cf2f9e449
Tiago Antolagic Duarte <tiago.antolagic-duarte@telefonica.com>
2025-07-13 15:01:17 +0200

    put the map functionallity etc. in one component

--- apps/frontend/scooteqlf02/src/App.vue
+++ apps/frontend/scooteqlf02/src/App.vue
@@ -1,40 +1,15 @@
 <script>
 import L from "leaflet";
+import OpenMap from "./components/OpenMap.vue";
 export default {
+  components: {
+    OpenMap,
+  },
   data() {
-    return {
-      map: null,
-    };
-  },
-  methods: {
-    addNewMarker() {
-      this.map.on("click", (e) => {
-        const lat = e.latlng.lat;
-        const lng = e.latlng.lng;
-        console.log("Geklickt bei:", lat, lng);
-
-        L.marker([lat, lng])
-          .addTo(this.map)
-          .bindPopup("Hier steht ein Scooter");
-      });
-    },
-  },
-
-  mounted() {
-    this.map = L.map("map").setView([52.52, 13.405], 13);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap contributors",
-    }).addTo(this.map);
-
-    L.marker([52.52, 13.405])
-      .addTo(this.map)
-      .bindPopup("Hier steht ein Scooter");
-    //.openPopup();
-    this.addNewMarker();
+    return {};
   },
 };
 </script>
 <template>
-  <div id="map" style="height: 400px; width: 600px"></div>
+  <OpenMap></OpenMap>
 </template>
---
+++ apps/frontend/scooteqlf02/src/components/OpenMap.vue
@@ -0,0 +1,40 @@
+<script>
+import L from "leaflet";
+export default {
+  data() {
+    return {
+      map: null,
+    };
+  },
+  methods: {
+    addNewMarker() {
+      this.map.on("click", (e) => {
+        const lat = e.latlng.lat;
+        const lng = e.latlng.lng;
+        console.log("Geklickt bei:", lat, lng);
+
+        L.marker([lat, lng])
+          .addTo(this.map)
+          .bindPopup("Hier steht ein Scooter");
+      });
+    },
+  },
+
+  mounted() {
+    this.map = L.map("map").setView([52.52, 13.405], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap contributors",
+    }).addTo(this.map);
+
+    L.marker([52.52, 13.405])
+      .addTo(this.map)
+      .bindPopup("Hier steht ein Scooter");
+    //.openPopup();
+    this.addNewMarker();
+  },
+};
+</script>
+<template>
+  <div id="map" style="height: 400px; width: 600px"></div>
+</template>
