c6b15c155c8483315a6ea881ddcee3d504c9e10b
Tiago Antolagic Duarte <tiago.antolagic-duarte@telefonica.com>
2025-07-13 14:51:39 +0200

    added method to make new markers where a broken scooter is

--- apps/frontend/scooteqlf02/src/App.vue
+++ apps/frontend/scooteqlf02/src/App.vue
@@ -2,18 +2,39 @@
 import L from "leaflet";
 export default {
   data() {
-    return {};
+    return {
+      map: null,
+    };
   },
-  methods: {},
+  methods: {
+    addNewMarker() {
+      this.map.on("click", (e) => {
+        const lat = e.latlng.lat;
+        const lng = e.latlng.lng;
+        console.log("Geklickt bei:", lat, lng);
+
+        L.marker([lat, lng])
+          .addTo(this.map)
+          .bindPopup("Hier steht ein Scooter");
+      });
+    },
+  },
 
   mounted() {
-    const map = L.map("map").setView([52.52, 13.405], 13);
+    this.map = L.map("map").setView([52.52, 13.405], 13);
+
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "© OpenStreetMap contributors",
-    }).addTo(map);
+    }).addTo(this.map);
+
+    L.marker([52.52, 13.405])
+      .addTo(this.map)
+      .bindPopup("Hier steht ein Scooter");
+    //.openPopup();
+    this.addNewMarker();
   },
 };
 </script>
 <template>
-  <div id="map" style="height: 400px"></div>
+  <div id="map" style="height: 400px; width: 600px"></div>
 </template>
